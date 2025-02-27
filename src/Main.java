a03f1de0cc5f5b21ffb3cf3a718cb487265fba61
ajssal20 <ajssal20@htl-shkoder.com>
2025-02-27 10:55:40 +0100

    Hi nedit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+abstract class Pyramide {
+    private double hohe;
+    public abstract double volumen();
+    public Pyramide(double hohe) {
+        this.hohe = hohe;    }
+    public double getHohe() {
+        return hohe;    }
+}
